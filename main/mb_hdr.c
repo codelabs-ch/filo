b3be97f4fc5db1e1039af2067460a56475f77677
Mathias Krause <mathias.krause@secunet.com>
2011-06-07 13:53:31 +0200

    Add multiboot header
    Change-Id: Ibe8e879a3a6f533f97aa8381697afb1f14d1f87d
    Signed-off-by: Mathias Krause <mathias.krause@secunet.com>
    Signed-off-by: Patrick Georgi <patrick.georgi@secunet.com>
    Reviewed-on: http://review.coreboot.org/197
    Tested-by: build bot (Jenkins)
    Reviewed-by: Patrick Georgi <patrick@georgi-clan.de>

---
+++ main/mb_hdr.c
@@ -0,0 +1,32 @@
+/*
+ * This file is part of FILO.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+/* Support for MultiBoot */
+#define MB_HEADER_MAGIC		0x1BADB002
+#define MB_RQ_FLAGS_4K		1
+#define MB_REQUESTED_FLAGS	(MB_RQ_FLAGS_4K)
+
+/* this header is parsed by the boot loader that loads FILO, e.g. GRUB */
+struct mb_header {
+	long magic;		/* multiboot magic */
+	long features;	/* requested features */
+	long chksum;	/* chksum for whole structure */
+} mb_header __attribute__((section(".hdr.mb"))) = {
+	.magic = MB_HEADER_MAGIC,
+	.features = MB_REQUESTED_FLAGS,
+	.chksum = -(MB_HEADER_MAGIC + MB_REQUESTED_FLAGS),
+};
