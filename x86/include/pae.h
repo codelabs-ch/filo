a7f4d776f3371e712a9fe8353586b1300ce71352
Reto Buerki <reet@codelabs.ch>
2020-05-04 10:55:16 +0200

    PAE paging support for FILO
    Port of the coreboot pae/pgtbl code to FILO.
    
    Provides PAE-capable memset and read functions, which are able to reach
    memory above 4 GiB. In order to keep FILO running during paging, the
    whole 32-bit address space is identity mapped, and a 2 MiB virtual
    address window is used to reach memory at a given physical address.
    
    The virtual address window is chosen not to overlap with linear
    addresses used by FILO. Therefore, the _start address before relocation
    is used internally.

---
+++ x86/include/pae.h
@@ -0,0 +1,33 @@
+/*
+ * This file is part of FILO.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#ifndef X86_PAE_H
+#define X86_PAE_H
+
+#include <stdint.h>
+
+/* Fill memory specified by physical address and length with a constant byte */
+void memset_pae(uint64_t dest, unsigned char pat, uint64_t length);
+
+/*
+ * Use given function read_func to read length bytes from buf to physical
+ * address dest
+ */
+int64_t read_pae(uint64_t dest, uint64_t length,
+		 int (*read_func)(void *buf, unsigned long len));
+
+#endif /* X86_PAE_H  */
